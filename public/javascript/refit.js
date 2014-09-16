011d4cf3f1509bbcc1404b161b7a40c4401c4931
webrefit <web.refit.lt@gmail.com>
2014-09-16 15:10:14 +0300

    keyword 'remontas' + subtitle

--- public/javascript/refit.js
+++ public/javascript/refit.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    
     document.getElementById("services_link").addEventListener("click", function(e) {
         document.getElementById('services').scrollIntoView();
     });
